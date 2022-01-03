b6283d9023a8107bf60439b0f94e3398d3f3facc
Micah Smith <86262666+MicahWSmith@users.noreply.github.com>
2022-01-03 12:15:06 -0500

    login commit

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
 app.get('/', (req,res) => {
     res.redirect("/0");
+})
+
+app.get('/login', (req,res) => {
+    res.render("index");
+})
+
+app.post("/signup", (req,res)=>{
+    const user = req.body.username;
+    res.send(`Welcome ${user}`);
 })
 
 app.get('/:riches', (req,res) => {
